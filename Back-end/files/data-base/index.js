6cd900bb765b350bee6b960327418a36fa41f544
Marcus-vinicius-pcs <83514936+Marcus-vinicius-pcs@users.noreply.github.com>
2022-02-21 19:02:59 -0300

    template get banco de dados

--- Back-end/files/data-base/index.js
+++ Back-end/files/data-base/index.js
@@ -1,4 +1,19 @@
 const app = require('express')
 const express = express();
+const mysql = require('mysql')
+
+const db = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "produtos"
+})
+
+app.get('/test', (req, res) => {
+    let SQL = "INSERT INTO produtos ( name, cost, category ) VALUES ( 'Camiseta', '60', 'Roupa')"
+    db.query(SQL, (err, result) => {
+        console.log(err)
+    })
+})
 
 app.use(express.json());
