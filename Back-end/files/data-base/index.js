e925d224cd65ae74d2204ed2f16cfaa62f7c557d
Marcus-vinicius-pcs <83514936+Marcus-vinicius-pcs@users.noreply.github.com>
2022-02-22 20:40:17 -0300

    versão funcional de salvar um objeto no mysql

--- Back-end/files/data-base/index.js
+++ Back-end/files/data-base/index.js
@@ -1,6 +1,6 @@
-const appDb = require('express')
-const express = express();
-const mysql = require('mysql')
+const express = require('express')
+const app = express()
+const mysql = require('mysql');
 
 const db = mysql.createPool({
     host: "localhost",
@@ -9,11 +9,13 @@ const db = mysql.createPool({
     database: "produtos"
 })
 
-appDb.get('/test', (req, res) => {
-    let SQL = "INSERT INTO produtos ( name, cost, category ) VALUES ( 'Camiseta', '60', 'Roupa')"
+app.get('/', (req, res) => {
+    let SQL = 
+    "INSERT INTO prod ( name, cost, category ) VALUES ( 'Camiseta', '60', 'Roupa')";
+
     db.query(SQL, (err, result) => {
         console.log(err)
     })
 })
 
-appDb.use(express.json());
+module.exports = app
--- Back-end/files/src/app.js
+++ Back-end/files/src/app.js
@@ -3,13 +3,13 @@ const app = express();
 const cors = require('cors');
 const formRoutes = require('./routes/form.routes')
 const dotenv = require('dotenv').config();
-
+const dbApp = require('../data-base/index')
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.json({ type: 'application/vnd.api+json' }));
 app.use(cors());
 app.use('/', formRoutes)
-
+app.use('/test', dbApp)
 module.exports = app
 
